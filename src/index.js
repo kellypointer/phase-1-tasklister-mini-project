cb5cb103151fc0997918c1d9b1752f5ce6dded6d
Kelly Pointer <kellyjoanpointer@gmail.com>
2025-04-10 14:42:47 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = document.querySelector("#new-task-description");
+    const task = input.value;
+    buildToDo(task); // create the new list item
+  });
 });
+
+function buildToDo(task) {
+  const newItem = document.createElement("li");
+  newItem.textContent = task;
+
+  const list = document.querySelector("#list");
+  list.appendChild(newItem);
+}
